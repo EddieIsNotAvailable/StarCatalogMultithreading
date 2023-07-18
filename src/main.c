8b8b572ee21c2f88e1a93bf432bd0958d98bb636
EddieIsNotAvailable <throwaway9172@protonmail.com>
2023-07-17 20:21:52 -0500

    working

--- src/main.c
+++ src/main.c
@@ -31,108 +31,149 @@
 #include "utility.h"
 #include "star.h"
 #include "float.h"
+#include <pthread.h>
 
-#define NUM_STARS 100000 
+
+#define NUM_STARS 30000 
 #define MAX_LINE 1024
-#define DELIMITER " \t\n"
 
 struct Star star_array[ NUM_STARS ];
 
+double min_global = FLT_MAX;
+double max_global = FLT_MIN;
 
-void showHelp()
+uint32_t thread_ct = 1;
+uint32_t load = NUM_STARS;
+
+typedef struct
 {
-  printf("Use: findAngular [options]\n");
-  printf("Where options are:\n");
-  printf("-t          Number of threads to use\n");
-  printf("-h          Show this help\n");
+  uint32_t start;
+  uint32_t end;
+  uint64_t count;
+  double dist_sum;
+  double min;
+  double max;
+} ThreadArg;
+
+void * determineAverageAngularDistance( void * arg )
+{
+	ThreadArg *this = (ThreadArg*) arg;
+	uint32_t i, j, end = this->end;
+	double distance,count=0,dist_sum=0;
+
+	for ( i = this->start; i < end; i++ )
+	{
+	  for ( j = i+1; j < NUM_STARS; j++ )
+	  {
+        distance = calculateAngularDistance( star_array[i].RightAscension, star_array[i].Declination,
+                              star_array[j].RightAscension, star_array[j].Declination ) ;
+
+        count++;
+        dist_sum += distance;
+
+        if( this->min > distance ) this->min = distance;
+        if( this->max < distance ) this->max = distance;
+	  }
+	}
+	this->dist_sum = dist_sum;
+	this->count = count;
+	pthread_exit(NULL);
 }
 
-// 
-// Embarassingly inefficient, intentionally bad method
-// to calculate all entries one another to determine the
-// average angular separation between any two stars 
-float determineAverageAngularDistance( struct Star arr[] )
+double runThreads()
 {
-    double total = 0;
+	pthread_t threads[thread_ct];
+	ThreadArg *args[thread_ct];
+	uint32_t start=0,i;
 
-    int i, j;
-    for (i = 0; i < NUM_STARS - 1; i++)
-    {
-      for (j = 0; j < NUM_STARS - i - 1; j++)
-      {
-        double distance = calculateAngularDistance( arr[i].RightAscension, arr[j].Declination,
-                                                   arr[j].RightAscension, arr[j].Declination ) ;
-        total += distance;
-      }
-    }
-    return total / NUM_STARS;
+	for( i = 0; i < thread_ct; i++ )
+	{
+		args[i] = malloc(sizeof(ThreadArg));
+		start = i * load;
+		args[i]->start = start;
+		args[i]->end = start + load;
+		args[i]->min = FLT_MAX;
+		args[i]->max = FLT_MIN;
+		pthread_create(&threads[i], NULL, determineAverageAngularDistance, (void *) args[i]);
+	}
+	
+	double dist_sum=0,count_sum=0;
+	for( i = 0; i < thread_ct; i++ )
+	{
+		pthread_join(threads[i], NULL);
+		if (args[i]->min<min_global) min_global = args[i]->min;
+		if (args[i]->max>max_global) max_global = args[i]->max;
+		count_sum += args[i]->count;
+		dist_sum += args[i]->dist_sum;
+		free(args[i]);
+	}
+
+	return dist_sum / count_sum;
 }
 
 
 int main( int argc, char * argv[] )
 {
+	struct timespec start, end;
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	FILE *fp;
+	uint32_t star_count=0,n,column;
 
-  FILE *fp;
-  uint32_t star_count = 0;
+	for( n = 1; n < argc; n++ )
+	{
+		if( strcmp(argv[n], "-t")==0)
+		{
+			thread_ct = atoi(argv[n+1]);
+			load = load/thread_ct;
+		}
+	}
 
-  uint32_t n;
+	fp = fopen( "data/tycho-trimmed.csv", "r" );
 
-  for( n = 1; n < argc; n++ )          
-  {
-    if( strcmp(argv[n], "-help" ) == 0 )
-    {
-      showHelp();
-      exit(0);
-    }
+	if( fp == NULL )
+	{
+		printf("ERROR: Unable to open the file data/tycho-trimmed.csv\n");
+		exit(1);
+	}
+
+	char line[MAX_LINE];
+	while (fgets(line, 1024, fp))
+	{
+		column=0;
+		char* tok;
+		for (tok = strtok(line, " ");
+				tok && *tok;
+				tok = strtok(NULL, " "))
+		{
+			switch( column )
+			{
+				case 0:
+					star_array[star_count].ID = atoi(tok);
+					break;
+				case 1:
+					star_array[star_count].RightAscension = atof(tok);
+					break;
+				case 2:
+					star_array[star_count].Declination = atof(tok);
+					break;
+				default: 
+					printf("ERROR: line %d had more than 3 columns\n", star_count );
+					exit(1);
+					break;
+			}
+			column++;
+		}
+		star_count++;
   }
+	printf("%d records read\n", star_count );
 
-  fp = fopen( "data/tycho-trimmed.csv", "r" );
+	double dist = runThreads();
 
-  if( fp == NULL )
-  {
-    printf("ERROR: Unable to open the file data/tycho-trimmed.csv\n");
-    exit(1);
-  }
+	clock_gettime(CLOCK_MONOTONIC, &end);
 
-  char line[MAX_LINE];
-  while (fgets(line, 1024, fp))
-  {
-    uint8_t column = 0;
-
-    char* tok;
-    for (tok = strtok(line, " ");
-            tok && *tok;
-            tok = strtok(NULL, " "))
-    {
-       switch( column )
-       {
-          case 0:
-              star_array[star_count].ID = atoi(tok);
-              break;
-       
-          case 1:
-              star_array[star_count].RightAscension = atof(tok);
-              break;
-       
-          case 2:
-              star_array[star_count].Declination = atof(tok);
-              break;
-
-          default: 
-             printf("ERROR: line %d had more than 3 columns\n", star_count );
-             exit(1);
-             break;
-       }
-       column++;
-    }
-    star_count++;
-  }
-  printf("%d records read\n", star_count );
-
-  // Find the average angular distance in the most inefficient way possible
-  double distance =  determineAverageAngularDistance( star_array );
-  printf("Average distance found is %lf\n", distance / NUM_STARS );
-
-  return 0;
+	printf("Average distance found is %f\n", dist );
+	printf("Minimum distance found is %lf\n", min_global );
+	printf("Maximum distance found is %lf\n", max_global );
+	printf("Seconds taken: %lf\n", (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0);
+	return 0;
 }
-
